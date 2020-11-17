c821e5bc6a93692237a729799c6c2d8c35f27447
vitorDaynno <vitordaynno@gmail.com>
2020-11-17 00:40:27 -0300

    Init add zac task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTrello, FaRegSadTear } from 'react-icons/fa';
 
 import { formatDate } from '../helpers/formatHelper';
 
@@ -6,19 +7,29 @@ function Task(props) {
   const { name, date, url } = props;
   return (
     <div className="row task">
-      {url ? (
-        <a
-          href={url}
-          target="_blank"
-          className="black-text"
-          rel="noopener noreferrer"
-        >
-          <h6 className="truncate title-task">{name}</h6>
-        </a>
-      ) : (
-        <h6 className="truncate title-task">{name}</h6>
-      )}
-      <span className="right date-task">{formatDate(date)}</span>
+      <div className="">
+        {url ? (
+          <div>
+            <h6 className="truncate col s10">{name}</h6>
+            <a
+              href={url}
+              target="_blank"
+              className="black-text"
+              rel="noopener noreferrer"
+            >
+              <FaTrello className="col s2" style={{ marginTop: '5px' }} />
+            </a>
+          </div>
+        ) : (
+          <div>
+            <h6 className="truncate col s10">{name}</h6>
+            <FaRegSadTear className="col s2" style={{ marginTop: '5px' }} />
+          </div>
+        )}
+        <div>
+          <span className="right date-task">{formatDate(date)}</span>
+        </div>
+      </div>
     </div>
   );
 }
